94d48d9bb7c3ad654ae400cb4cf119c85ca8afa5
De Vinne <vinne704@gmail.com>
2026-05-13 22:29:45 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,13 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({
+        "status": "success",
+        "message": "Hello from Vercel Python Backend!"
+    })
+
+if __name__ == "__main__":
+    app.run()
